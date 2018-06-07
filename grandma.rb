10ad601e1594bfecf3867dbaf03b547de5f5b06f
richardojo04@gmail.com <richardojo04@gmail.com>
2018-06-07 00:47:27 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,6 @@
 def speak_to_grandma(phrase)
  if phrase == "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN!"
-  
-  if phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
   elsif phrase != phrase.upcase
   return "HUH?! SPEAK UP, SONNY!"
   else phrase == phrase.upcase
